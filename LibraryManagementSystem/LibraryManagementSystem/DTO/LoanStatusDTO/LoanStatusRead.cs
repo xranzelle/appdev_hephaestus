9c675c544a1eb8f92686d327619f3f9681153b24
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-09-24 13:39:01 +0800

    Update LoanStatusRead.cs on LoanStatusReadDTO
    loan status read get; set; updated please check

--- LibraryManagementSystem/LibraryManagementSystem/DTO/LoanStatusDTO/LoanStatusRead.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/LoanStatusDTO/LoanStatusRead.cs
@@ -2,5 +2,10 @@
 {
     public class LoanStatusRead
     {
+        public int StatusId { get; set; }
+
+        public string StatusName { get; set; } = string.Empty;
+
+        public List<LoanDTO> Loans { get; set; } = new();
     }
 }
