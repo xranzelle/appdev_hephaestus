d3d2840359414bf8bc6c070f644eeaa1822517a9
vinakristinefrancisco <vinakrstn01@gmail.com>
2025-09-30 23:33:13 +0800

    Update BooksController.cs

--- LibraryManagementSystem/LibraryManagementSystem/Controllers/BooksController.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Controllers/BooksController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryManagementSystem.Models;
+using AutoMapper;
+using LibraryManagementSystem.DTO.BooksDTO;
 
 namespace LibraryManagementSystem.Controllers
 {
@@ -14,22 +11,27 @@ namespace LibraryManagementSystem.Controllers
     public class BooksController : ControllerBase
     {
         private readonly LibraryDbContext _context;
+        private readonly IMapper _mapper;
 
-        public BooksController(LibraryDbContext context)
+        public BooksController(LibraryDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: api/Books
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public async Task<ActionResult<IEnumerable<BooksRead>>> GetBooks()
         {
-            return await _context.Books.ToListAsync();
+            var books = await _context.Books.OrderBy(b => b.BookId).ToListAsync();
+            var mappedBooks = _mapper.Map<List<BooksRead>>(books);
+
+            return Ok(mappedBooks);
         }
 
         // GET: api/Books/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
+        public async Task<ActionResult<BooksRead>> GetBook(int id)
         {
             var book = await _context.Books.FindAsync(id);
 
@@ -38,20 +40,22 @@ namespace LibraryManagementSystem.Controllers
                 return NotFound();
             }
 
-            return book;
+            var mappedBook = _mapper.Map<BooksRead>(book);
+            return Ok(mappedBook);
         }
 
         // PUT: api/Books/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBook(int id, Book book)
+        public async Task<IActionResult> PutBook(int id, BooksPut bookDTO)
         {
-            if (id != book.BookId)
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
             {
-                return BadRequest();
+                return NotFound();
             }
 
-            _context.Entry(book).State = EntityState.Modified;
+            _mapper.Map(bookDTO, book);
 
             try
             {
@@ -59,7 +63,8 @@ namespace LibraryManagementSystem.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!BookExists(id))
+                var exists = await _context.Books.AnyAsync(e => e.BookId == id);
+                if (!exists)
                 {
                     return NotFound();
                 }
@@ -69,14 +74,15 @@ namespace LibraryManagementSystem.Controllers
                 }
             }
 
-            return NoContent();
+            return Ok(book);
         }
 
         // POST: api/Books
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Book>> PostBook(Book book)
+        public async Task<ActionResult<BooksRead>> PostBook(BooksPost bookDTO)
         {
+            var book = _mapper.Map<Book>(bookDTO);
+
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
 
@@ -97,11 +103,6 @@ namespace LibraryManagementSystem.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
-        }
-
-        private bool BookExists(int id)
-        {
-            return _context.Books.Any(e => e.BookId == id);
         }
     }
 }
