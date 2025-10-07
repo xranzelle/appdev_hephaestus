9081fe6ccad0aca515096fb360cee0e5afc08447
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-07 23:14:36 +0800

    Update MembersRead.cs

--- LibraryManagementSystem/LibraryManagementSystem/DTO/MembersDTO/MembersRead.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/MembersDTO/MembersRead.cs
@@ -5,11 +5,5 @@ namespace LibraryManagementSystem.DTO.MembersDTO
         public int MemberId { get; set; }
 
         public string Name { get; set; } = null!;
-
-        public string Email { get; set; } = null!;
-
-        public string? ContactNumber { get; set; }
-
-        public string? Address { get; set; }
     }
 }
