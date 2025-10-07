f0e20cb17042d867acc249953dac1e97344723bb
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-07 23:16:50 +0800

    Update MembersPut.cs
    please double check

--- LibraryManagementSystem/LibraryManagementSystem/DTO/MembersDTO/MembersPut.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/MembersDTO/MembersPut.cs
@@ -3,13 +3,5 @@ namespace LibraryManagementSystem.DTO.MembersDTO
     public class MembersPut
     {
         public string Name { get; set; } = null!;
-
-        public string Email { get; set; } = null!;
-
-        public string? ContactNumber { get; set; }
-
-        public string? Address { get; set; }
-
-        public DateOnly? MembershipDate { get; set; }
     }
 }
