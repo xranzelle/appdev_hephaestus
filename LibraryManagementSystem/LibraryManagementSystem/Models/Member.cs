ee737bf8f4e1845095efb0100d81dd5d9c6c2cee
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:34:25 +0800

    Update Member.cs
    added validation on Member.cs model, please double check

--- LibraryManagementSystem/LibraryManagementSystem/Models/Member.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Models/Member.cs
@@ -1,18 +1,31 @@
-﻿namespace LibraryManagementSystem.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-public partial class Member
+namespace LibraryManagementSystem.Models
 {
-    public int MemberId { get; set; }
+    public partial class Member
+    {
+        public int MemberId { get; set; }
 
-    public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "Member name is required.")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters.")]
+        public string Name { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
+        [StringLength(100, ErrorMessage = "Email can't be longer than 100 characters.")]
+        public string Email { get; set; } = null!;
 
-    public string? ContactNumber { get; set; }
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        [StringLength(15, ErrorMessage = "Contact number can't be longer than 15 characters.")]
+        public string? ContactNumber { get; set; }
 
-    public string? Address { get; set; }
+        [StringLength(250, ErrorMessage = "Address can't be longer than 250 characters.")]
+        public string? Address { get; set; }
 
-    public DateOnly MembershipDate { get; set; }
+        [Required(ErrorMessage = "Membership date is required.")]
+        public DateOnly MembershipDate { get; set; }
 
-    public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+        public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
 }
