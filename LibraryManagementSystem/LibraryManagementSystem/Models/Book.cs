6e8d004be6ca901512c3de41e23908a9f8504834
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:28:39 +0800

    Update Book.cs
    added validation on book.cs model, please double check

--- LibraryManagementSystem/LibraryManagementSystem/Models/Book.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Models/Book.cs
@@ -1,22 +1,30 @@
-﻿namespace LibraryManagementSystem.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-public partial class Book
+namespace LibraryManagementSystem.Models
 {
-    public int BookId { get; set; }
+    public partial class Book
+    {
+        public int BookId { get; set; }
 
-    public string Title { get; set; } = null!;
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(150, ErrorMessage = "Title can't be longer than 150 characters.")]
+        public string Title { get; set; } = null!;
 
-    public int AuthorId { get; set; }
+        [Required(ErrorMessage = "Author ID is required.")]
+        public int AuthorId { get; set; }
 
-    public int GenreId { get; set; }
+        [Required(ErrorMessage = "Genre ID is required.")]
+        public int GenreId { get; set; }
 
-    public int? PublishedYear { get; set; }
+        [Range(0, 2100, ErrorMessage = "Published year must be a valid year.")]
+        public int? PublishedYear { get; set; }
 
-    public string? Isbn { get; set; }
+       [StringLength(13, MinimumLength = 10, ErrorMessage = "ISBN must be between 10 and 13 characters.")]
+        public string? Isbn { get; set; }
 
-    public virtual Author Author { get; set; } = null!;
-
-    public virtual Genre Genre { get; set; } = null!;
-
-    public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+        public virtual Author Author { get; set; } = null!;
+        public virtual Genre Genre { get; set; } = null!;
+        public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
 }
