555add295682fab0b45341331f8b9672e7ce2541
xranzelle <ranzellepaulino08@gmail.com>
2025-10-08 22:47:50 +0800

    Revert "Update AuthorsPost.cs"

--- LibraryManagementSystem/LibraryManagementSystem/DTO/AuthorsDTO/AuthorsPost.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/AuthorsDTO/AuthorsPost.cs
@@ -1,17 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace LibraryManagementSystem.DTO.AuthorsDTO
+﻿namespace LibraryManagementSystem.DTO.AuthorsDTO
 {
     public class AuthorsPost
     {
-        [Required(ErrorMessage = "Name is required.")]
-        [StringLength(25, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 25 characters.")]
         public string Name { get; set; } = null!;
 
-        [StringLength(25, ErrorMessage = "Nationality can't be longer than 25 characters.")]
         public string? Nationality { get; set; }
 
-        [DataType(DataType.Date)]
         public DateOnly? Birthdate { get; set; }
     }
 }
