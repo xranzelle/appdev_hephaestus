aec5e7de1165844658b31d83edaf4a76ccce1494
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:11:04 +0800

    Update LoansPost.cs
    LoansPost.cs updated, please double check

--- LibraryManagementSystem/LibraryManagementSystem/DTO/LoansDTO/LoansPost.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/LoansDTO/LoansPost.cs
@@ -13,18 +13,6 @@ namespace LibraryManagementSystem.DTO.LoansDTO
         public int BookId { get; set; }
 
         [Required(ErrorMessage = "DueDate is required.")]
-        [DataType(DataType.Date)]
-        [CustomValidation(typeof(LoansPost), nameof(ValidateDueDate))]
         public DateOnly DueDate { get; set; }
-
-        // Custom validation method to ensure DueDate is in the future (or today)
-        public static ValidationResult? ValidateDueDate(DateOnly dueDate, ValidationContext context)
-        {
-            if (dueDate < DateOnly.FromDateTime(DateTime.Today))
-            {
-                return new ValidationResult("DueDate cannot be in the past.");
-            }
-            return ValidationResult.Success;
-        }
     }
 }
