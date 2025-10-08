d7bc787c4b955a737ebc70d6a3c362770202d533
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:09:32 +0800

    Update LoansPut.cs
    updated the loansPut.cs please double check

--- LibraryManagementSystem/LibraryManagementSystem/DTO/LoansDTO/LoansPut.cs
+++ LibraryManagementSystem/LibraryManagementSystem/DTO/LoansDTO/LoansPut.cs
@@ -4,24 +4,11 @@ namespace LibraryManagementSystem.DTO.LoansDTO
 {
     public class LoansPut
     {
-        //testing if nagana ba yung approval ni ranz
         [Required(ErrorMessage = "DueDate is required.")]
-        [DataType(DataType.Date)]
-        [CustomValidation(typeof(LoansPut), nameof(ValidateDueDate))]
         public DateOnly DueDate { get; set; }
 
         [Required(ErrorMessage = "StatusId is required.")]
         [Range(1, int.MaxValue, ErrorMessage = "StatusId must be a positive integer.")]
         public int StatusId { get; set; }
-
-        // Custom validation method to ensure DueDate is today or in the future
-        public static ValidationResult? ValidateDueDate(DateOnly dueDate, ValidationContext context)
-        {
-            if (dueDate < DateOnly.FromDateTime(DateTime.Today))
-            {
-                return new ValidationResult("DueDate cannot be in the past.");
-            }
-            return ValidationResult.Success;
-        }
     }
 }
