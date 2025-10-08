9e5f18a27a48ec17b2004ded16d54460f59bf181
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:32:49 +0800

    Update LoanStatus.cs
    added validation on LoanStatus.cs, please double check

--- LibraryManagementSystem/LibraryManagementSystem/Models/LoanStatus.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Models/LoanStatus.cs
@@ -1,10 +1,16 @@
-﻿namespace LibraryManagementSystem.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-public partial class LoanStatus
+namespace LibraryManagementSystem.Models
 {
-    public int StatusId { get; set; }
+    public partial class LoanStatus
+    {
+        public int StatusId { get; set; }
 
-    public string StatusName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Status name is required.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Status name must be between 3 and 30 characters.")]
+        public string StatusName { get; set; } = string.Empty;
 
-    public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+        public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+    }
 }
