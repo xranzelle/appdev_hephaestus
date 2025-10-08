0a5a916ff0ba60345c187ac3af6dc713285766ad
schrodingersCat720 <jerielsamuelsuson719@gmail.com>
2025-10-08 23:31:54 +0800

    Update Loan.cs
    added a validation on loan.cs model, please double check

--- LibraryManagementSystem/LibraryManagementSystem/Models/Loan.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Models/Loan.cs
@@ -1,24 +1,30 @@
-﻿namespace LibraryManagementSystem.Models;
+using System.ComponentModel.DataAnnotations;
 
-public partial class Loan
+namespace LibraryManagementSystem.Models
 {
-    public int LoanId { get; set; }
+    public partial class Loan
+    {
+        public int LoanId { get; set; }
 
-    public int BookId { get; set; }
+        [Required(ErrorMessage = "Book ID is required.")]
+        public int BookId { get; set; }
 
-    public int MemberId { get; set; }
+        [Required(ErrorMessage = "Member ID is required.")]
+        public int MemberId { get; set; }
 
-    public DateOnly LoanDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [Required(ErrorMessage = "Loan date is required.")]
+        public DateOnly LoanDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
-    public DateOnly DueDate { get; set; }
+        [Required(ErrorMessage = "Due date is required.")]
+        public DateOnly DueDate { get; set; }
 
-    public DateOnly? ReturnDate { get; set; }
+        public DateOnly? ReturnDate { get; set; }
 
-    public int StatusId { get; set; } = 1;
+        [Required(ErrorMessage = "Status ID is required.")]
+        public int StatusId { get; set; } = 1;
 
-    public virtual Book Book { get; set; } = null!;
-
-    public virtual Member Member { get; set; } = null!;
-
-    public virtual LoanStatus Status { get; set; } = null!;
+        public virtual Book Book { get; set; } = null!;
+        public virtual Member Member { get; set; } = null!;
+        public virtual LoanStatus Status { get; set; } = null!;
+    }
 }
